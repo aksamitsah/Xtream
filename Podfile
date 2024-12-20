aae7b0123afe23ff8e0506e91d0d12b9ff33abf0
Amit Sah <aksamitsah@gmail.com>
2024-12-21 04:14:55 +0530

    update to SPM
    update to SPM

--- Podfile
+++
@@ -1,9 +0,0 @@
-platform :ios, '15.0'
-
-target 'xtream' do
-
-  use_frameworks!
-  # Pods for xtream
-  pod 'lottie-ios'
-
-end
--- ci_scripts/ci_post_clone.sh
+++
@@ -1,9 +0,0 @@
-#
-//  ci_post_clone.sh
-//  xtream
-//
-//  Created by Amit Shah on 21/12/24.
-//
-
-brew install cocoapods
-pod install
